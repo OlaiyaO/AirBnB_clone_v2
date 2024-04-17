fce5a1fb11b10ed5465c0484e60ea325462167c6
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-04-17 21:48:45 +0100

    db_storage.py

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -14,23 +14,23 @@ from models.amenity import Amenity
 
 class DatabaseStorage:
     """Manage SQL Alchemy database."""
-    __db_engine = None
-    __db_session = None
+    __engine = None
+    __session = None
 
     def __init__(self):
         """Initialize database connection."""
-        user = getenv("HBNB_DB_USER")
-        passwd = getenv("HBNB_DB_PASSWORD")
-        db = getenv("HBNB_DB_NAME")
-        host = getenv("HBNB_DB_HOST")
-        env = getenv("HBNB_ENVIRONMENT")
+        user = getenv("HBNB_MYSQL_USER")
+        password = getenv("HBNB_MYSQL_PWD")
+        database = getenv("HBNB_MYSQL_DB")
+        host = getenv("HBNB_MYSQL_HOST")
+        env = getenv("HBNB_ENV")
 
-        self.__db_engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
-                                         .format(user, passwd, host, db),
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'
+                                         .format(user, passwd, host, database),
                                          pool_pre_ping=True)
 
         if env == "test":
-            Base.metadata.drop_all(self.__db_engine)
+            Base.metadata.drop_all(self.__engine)
 
     def get_all(self, cls=None):
         """Get all objects from database."""
@@ -38,14 +38,14 @@ class DatabaseStorage:
         if cls:
             if type(cls) is str:
                 cls = eval(cls)
-            query = self.__db_session.query(cls)
+            query = self.__session.query(cls)
             for obj in query:
                 key = "{}.{}".format(type(obj).__name__, obj.id)
                 obj_dict[key] = obj
         else:
             classes = [State, City, User, Place, Review, Amenity]
             for cls in classes:
-                query = self.__db_session.query(cls)
+                query = self.__session.query(cls)
                 for obj in query:
                     key = "{}.{}".format(type(obj).__name__, obj.id)
                     obj_dict[key] = obj
@@ -53,23 +53,23 @@ class DatabaseStorage:
 
     def add(self, obj):
         """Add new object to database."""
-        self.__db_session.add(obj)
+        self.__session.add(obj)
 
     def save(self):
         """Save changes to the database."""
-        self.__db_session.commit()
+        self.__session.commit()
 
     def delete(self, obj=None):
         """Delete an object from the database."""
         if obj:
-            self.__db_session.delete(obj)
+            self.__session.delete(obj)
 
     def reload(self):
         """Reload the database."""
-        Base.metadata.create_all(self.__db_engine)
-        session_factory = sessionmaker(bind=self.__db_engine, expire_on_commit=False)
+        Base.metadata.create_all(self.__engine)
+        session_factory = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session_factory)
-        self.__db_session = Session()
+        self.__session = Session()
 
     def close(self):
         """Close the database connection."""
