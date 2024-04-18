b5b5d895a2d4af98d4305126ac96cecdbe8f3975
Joe Black <63033475+OlaiyaO@users.noreply.github.com>
2024-04-18 04:04:41 +0100

    Update review.py

--- models/review.py
+++ models/review.py
@@ -1,16 +1,12 @@
 #!/usr/bin/python3
-"""This is the review class"""
+"""This is the review class definition"""
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String, ForeignKey, Float
 
 
 class Review(BaseModel, Base):
-    """This is the class for Review
-    Attributes:
-        place_id: place id
-        user_id: user id
-        text: review description
+    """This is the class for Review definition 
     """
     __tablename__ = "reviews"
     text = Column(String(1024), nullable=False)
