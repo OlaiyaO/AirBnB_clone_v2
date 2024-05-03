e2fcb15b3c04340988313aa33dc2255f86144468
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-03 16:31:27 +0100

    Let me see if it'll work

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,16 +1,14 @@
 #!/usr/bin/python3
 """
-A scrpt that distributes an archive to web servers using the function.
+Fabric of an archive to your web servers using the function do_deploy.
 """
+
+
 from fabric.api import env, put, run
 from os.path import exists
 import os
 
-
-# Define the list of web server IPs
-env.hosts = ['54.237.115.176', '54.234.13.131']
-# Define the SSH username
-env.user = 'ubuntu'
+env.hosts = ["54.237.115.176", "54.234.13.131"]
 
 def do_deploy(archive_path):
     """
@@ -26,17 +24,19 @@ def do_deploy(archive_path):
         return False
 
     try:
-        # Upload the archive to the /tmp/ directory of the web server
-        put(archive_path, '/tmp/')
-        archive_name = archive_path.split('/')[-1]
-        release_path = '/data/web_static/releases/' + archive_name[:-4]
-        run('mkdir -p {}'.format(release_path))
-        run('tar -xzf /tmp/{} -C {}'.format(archive_name, release_path))
-        run('rm /tmp/{}'.format(archive_name))
-        run('mv {}/web_static/* {}'.format(release_path, release_path))
-        run('rm -rf {}/web_static'.format(release_path))
-        run('rm -rf /data/web_static/current')
-        run('ln -s {} /data/web_static/current'.format(release_path))
+        file = archive_path.split("/")[-1]
+        name = file.split(".")[0]
+
+        put(archive_path, "/tmp/{}".format(file))
+        run("rm -rf /data/web_static/releases/{}/".format(name))
+        run("mkdir -p /data/web_static/releases/{}/".format(name))
+        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
+        run("rm /tmp/{}".format(file))
+        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
+        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
+        run("rm -rf /data/web_static/current")
+        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
+
         print("New version deployed!")
         return True
     except Exception as e:
@@ -45,3 +45,4 @@ def do_deploy(archive_path):
 
 if __name__ == "__main__":
     do_deploy(archive_path)
+
