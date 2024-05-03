33b5cc87063748c2112e6c15b03672dadca28658
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-03 16:54:12 +0100

    Let me see if it'll work

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -9,43 +9,43 @@ from fabric.api import env, put, run
 
 # Define the list of web server IPs and the SSH username
 env.hosts = ["54.237.115.176", "54.234.13.131"]
-env.user = "ubuntu"
-
 def do_deploy(archive_path):
-    """
-    Distributes an archive to web servers.
+    """Distributes an archive to a web server.
 
     Args:
-        archive_path (str): Path of the archive to be deployed.
-
+        archive_path (str): The path of the archive to distribute.
     Returns:
-        bool: True if deployment is successful, False otherwise.
+        If the file doesn't exist at archive_path or an error occurs - False.
+        Otherwise - True.
     """
-    if not os.path.isfile(archive_path):
+    if os.path.isfile(archive_path) is False:
         return False
+    file = archive_path.split("/")[-1]
+    name = file.split(".")[0]
 
-    try:
-        file = os.path.basename(archive_path)
-        name = os.path.splitext(file)[0]
-
-        put(archive_path, "/tmp/{}".format(file))
-        run("rm -rf /data/web_static/releases/{}/".format(name))
-        run("mkdir -p /data/web_static/releases/{}/".format(name))
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(file, name))
-        run("rm /tmp/{}".format(file))
-        run("mv /data/web_static/releases/{}/web_static/* /data/web_static/releases/{}/".format(name, name))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(name))
-        run("rm -rf /data/web_static/current")
-        run("ln -s /data/web_static/releases/{}/ /data/web_static/current".format(name))
-
-        print("New version deployed!")
-        return True
-    except Exception as e:
-        print(e)
+    if put(archive_path, "/tmp/{}".format(file)).failed is True:
         return False
-
-if __name__ == "__main__":
-    # Example usage
-    archive_path = "your_archive_path_here"
-    do_deploy(archive_path)
+    if run("rm -rf /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("mkdir -p /data/web_static/releases/{}/".
+           format(name)).failed is True:
+        return False
+    if run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".
+           format(file, name)).failed is True:
+        return False
+    if run("rm /tmp/{}".format(file)).failed is True:
+        return False
+    if run("mv /data/web_static/releases/{}/web_static/* "
+           "/data/web_static/releases/{}/".format(name, name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/releases/{}/web_static".
+           format(name)).failed is True:
+        return False
+    if run("rm -rf /data/web_static/current").failed is True:
+        return False
+    if run("ln -s /data/web_static/releases/{}/ /data/web_static/current".
+           format(name)).failed is True:
+        return False
+    return True
 
