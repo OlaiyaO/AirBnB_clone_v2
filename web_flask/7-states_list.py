328e1baaff46a48da25b99e658c5fd837ab72d78
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-19 15:01:33 +0100

    fixing on

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
-"""
-This script starts a Flask web application.
+"""Starts a Flask web application.
 
-The application listens on 0.0.0.0, port 5000 and has the following route:
-    /states_list: Displays an HTML page with a list of all State objects in
-    DBStorage.
+The application listens on 0.0.0.0, port 5000.
+Routes:
+    /states_list: HTML page with a list of all State objects in DBStorage.
 """
-
 from models import storage
-from flask import Flask, render_template
+from flask import Flask
+from flask import render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Display an HTML page with a list of all State objects in DBStorage.
+    """Displays an HTML page with a list of all State objects in DBStorage.
 
     States are sorted by name.
     """
