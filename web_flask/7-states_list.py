f6fbf7d686d9caa4c2f548f43aca9aa36944d879
root <root@18ba9d38f8a1.d1d80cbc.alx-cod.online>
2024-05-20 03:02:22 +0100

    rounding up on 7-states

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,30 +1,21 @@
 #!/usr/bin/python3
-"""Starts a Flask web application.
-
-The application listens on 0.0.0.0, port 5000.
-Routes:
-    /states_list: HTML page with a list of all State objects in DBStorage.
-"""
+"""Starts a Flask web application on 0.0.0.0:5000 with route /states_list."""
 from models import storage
-from flask import Flask
-from flask import render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 
 @app.route("/states_list", strict_slashes=False)
 def states_list():
-    """Displays an HTML page with a list of all State objects in DBStorage.
-
-    States are sorted by name.
-    """
+    """Displays an HTML page with a list of all State objects."""
     states = storage.all("State")
     return render_template("7-states_list.html", states=states)
 
 
 @app.teardown_appcontext
 def teardown(exc):
-    """Remove the current SQLAlchemy session."""
+    """Closes the current SQLAlchemy session."""
     storage.close()
 
 
